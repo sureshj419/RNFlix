5c3c2e3b88871c8077b7b28cc871af329a260069
Suresh Jallipalli <suresh.jallipalli@rentokil-initial.com>
2025-10-29 20:18:13 +0000

    Changes for Home Page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native'
 import React from 'react'
 
-const Onboarding = () => {
+const Profile = () => {
     return (
         <View>
-            <Text>Onboarding</Text>
+            <Text>Profile</Text>
         </View>
     )
 }
-export default Onboarding
+export default Profile
 const styles = StyleSheet.create({})
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
 import { Stack } from "expo-router";
 import "./globals.css"
+import Search from "@/app/(tabs)/search";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+      <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+  </Stack>
+  );
 }
